de9850b2368eefb616929ee96ae0560202425fac
Nikhil Massand <nikhilmassand@Nikhils-MBP.cable.rcn.com>
2021-04-27 22:07:46 -0400

    started supertest testing and completed reducers testing for now

---
+++ __tests__/mainReducer.js
@@ -0,0 +1,143 @@
+import subject from '../client/state/reducers/mainReducer';
+
+
+
+describe('Main Reducer tests', () => {
+	let state;
+
+	beforeEach(() => {
+		state = {
+			user: null,
+			login_state: null,
+			signup_state: null,
+			info: null,
+			messages: {},
+			view: 'userpage',
+			user_info: null
+		};
+	})
+
+	describe('default state', () => {
+    it ('should return the default state when input is undefined', () => {
+      expect(subject(undefined, {type: undefined})).toEqual(state);
+    });
+	});
+  
+	describe('unrecognized action types', () => {
+    it('should return the original state without duplication', () => {
+      const action = {type: 'awrilukjasndasloi'};
+      expect(subject(state,action)).toBe(state);
+    });
+	});
+  
+	describe('LOGIN_STATE', () => {
+		
+	})
+	describe('LOGIN', () => {
+		const action = {
+      type: 'LOGIN',
+      payload: {
+				user: {
+					_id: '45',
+					username: 'yo',
+					language: 'spanish'
+				},
+				messages: {message: 'message1'}
+			},
+		};
+		
+		it('updates the user state', () => {
+			const { messages, login_state, user } = subject(state, action);
+			expect(user).toEqual(action.payload.user); 
+
+		});
+		it('updates the messages state', () => {
+			const { messages, login_state, user } = subject(state, action);
+			expect(messages).toEqual(action.payload.messages);
+		});
+		it('updates the login state to true', () => {
+			const { messages, login_state, user } = subject(state, action);
+			expect(login_state).toBe('true');
+		});
+		it('returns a state object not strictly equal to the original', () => {
+			const { initialState } = subject(state, action);
+			expect(initialState).not.toBe(state);
+		})
+
+	})
+	
+	describe('SIGNUP_STATE', () => {
+		const action = {
+            type: 'SIGNUP_STATE',
+            payload: true
+        }
+        it('updates signup_state to be true', () => {
+            const { signup_state } = subject(state, action)
+            expect(signup_state).toBe(true);
+          });
+	})
+
+	describe('SIGNUP', () => {
+		const action = {
+            type: 'SIGNUP'
+        }
+        it('sets login_state to true', () => {
+            const { login_state } = subject(state, action)
+            expect(login_state).toBe(true);
+          });
+	})
+	describe('VIEW', () => {
+		const action = {
+      type: 'VIEW',
+      payload: 'userpage' // not sure if this is correct
+    }
+    it ('updates the current view to userpage', () => {
+      const { view } = subject(state, action);
+      expect(view).toEqual(action.payload);
+    })
+	})
+	describe('USER_INFO', () => {
+		 const action = {
+            type: 'USER_INFO',
+            payload: 'wrongPassword'
+        }
+        it('updates value of user info', () => {
+            const { user_info } = subject(state, action);
+            expect(user_info).toEqual('wrongPassword')
+          });
+	})
+	describe('UPDATE_MESSAGES', () => {
+    const action = {
+      type: 'UPDATE_MESSAGES',
+      payload: {
+        id: '67',
+        targetUsername: 'blabby',
+        senderUsername: 'coraline',
+        language: 'french',
+        message: 'I don\'t speak french'
+      } // need to figure out what this object looks like
+    }
+
+    it ('updates the messages on the UI -> id', () => {
+      const { messages } = subject(state, action);
+			console.log(subject(state, action))
+      expect(messages.id).toEqual('67');
+    })
+    it ('updates the messages on the UI -> targetUsername', () => {
+      const { messages } = subject(state, action);
+      expect(messages.targetUsername).toEqual('blabby');
+    })
+    it ('updates the messages on the UI -> senderUsername', () => {
+      const { messages } = subject(state, action);
+      expect(messages.senderUsername).toEqual('coraline');
+    })
+    it ('updates the messages on the UI -> language', () => {
+      const { messages } = subject(state, action);
+      expect(messages.language).toEqual('french');
+    })
+    it ('updates the messages on the UI -> message', () => {
+      const { messages } = subject(state, action);
+      expect(messages.message).toEqual('I don\'t speak french');
+    })
+	})
+})
---
+++ __tests__/supertest.js
@@ -0,0 +1,93 @@
+// const request = require('supertest');
+// const fs = require('fs');
+// const path = require('path');
+// const assert = require('assert');
+// const { findUserByCookie } = require('./../server/controllers/cookieController');
+// // console.log(findUserByCookie);
+
+// const server = 'http://localhost:3000';
+
+// //test for line 111 in server
+// describe('Route integration', () => {
+//     describe('/', () => {
+//       describe('GET', () => {
+//         // Note that we return the evaluation of `request` here! It evaluates to
+//         // a promise, so Jest knows not to say this test passes until that
+//         // promise resolves. See https://jestjs.io/docs/en/asynchronous
+//         it('responds with 200 status and text/html content type', () => {
+//           return request(server)
+//             .get('/')
+//             .expect('Content-Type', /text\/html/)
+//             .expect(200);
+//         });
+//       });
+//     });
+// })
+
+// //describe('Signup Test', () => {
+//     // describe('/signup', () => {
+//     //   describe('GET', () => {
+//     //     it('responds with 200 status and application/json content type', () => {
+//     //       return request(server)
+//     //         .get('/signup')
+//     //         .expect('Content-Type', /application\/json/)
+//     //         .expect(200);
+//     //     });
+//     //   });
+//     // });
+
+
+// describe('Get request for messages', () => {
+//     let cookies;
+//     describe('/messages', () => {
+//         describe('GET', () => {
+//           beforeAll((done) => {
+//             //test if cookie.ssid type of string and httpOnly type of boolean or is true  
+//             cookies = {
+//               ssid: 'j%3A%226088a1310deb7aa2a08c5462%22',
+//               httpOnly: true
+//             }
+//             user = {
+//               _id: 'j%3A%226088a1310deb7aa2a08c5462%22',
+//               messages: 'hi guys'
+//             }
+//           })
+//         it('checks the request to see if the cookie exists in the database', () => {
+//           return request(server)
+//             .get('/messages')
+//             .set('Cookie', ['ssid', cookies.ssid])
+//             .expect('Content-Type', /application\/json/)
+//             .expect(200);
+//                 // .end((err, res) => {
+//     //   if (err) {
+//     //     return done(err);
+//     //   }
+//     //   expect(res.locals.user).to.be.equal('user');
+//     //   return done();
+//         });
+//       });
+//     });
+// }
+
+
+
+
+//   // beforeEach(() => {
+//   //   state = {
+//   //     totalMarkets: 0,
+//   //     totalCards: 0,
+//   //     marketList: [],
+//   //     newLocation: '',
+//   //     synced: true,
+//   //   };
+//   // });
+
+
+// // it('responds with the updated market list', () => {
+// //     const testMarket = [{ location: 'New York', cards: 5 }];
+// //     return request(server)
+// //       .post('/markets')
+// //       .send(testMarket)
+// //       .then((response) => {
+// //         expect(response.body).toEqual(testMarket);
+// //       });
